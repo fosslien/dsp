f15aa2307dcd000055f67d0a343d5a4ed842cb62
Liz <e.fosslien@gmail.com>
2017-01-08 16:41:14 -0800

    Added code for all

--- python/q6_strings.py
+++ python/q6_strings.py
@@ -36,74 +36,61 @@ fix_start(string)
 
 
 def mix_up(a, b):
-    """
-    Given strings a and b, return a single string with a and b
-    separated by a space '<a> <b>', except swap the first 2 chars of
-    each string. Assume a and b are length 2 or more.
+    firsta = a[:2]
+    firstb = b[:2]
+    lasta = a[2:]
+    lastb = b[2:]
+    newa = firstb + lasta
+    newb = firsta + lastb
+    print newa, newb
 
-    >>> mix_up('mix', 'pod')
-    'pox mid'
-    >>> mix_up('dog', 'dinner')
-    'dig donner'
-    >>> mix_up('gnash', 'sport')
-    'spash gnort'
-    >>> mix_up('pezzy', 'firm')
-    'fizzy perm'
-    """
-    raise NotImplementedError
+first = raw_input("Enter a string with more than 2 characters: ")
+second = raw_input("Enter a string with more than 2 characters: ")
+
+mix_up(first,second)
 
 
 def verbing(s):
-    """
-    Given a string, if its length is at least 3, add 'ing' to its end.
-    Unless it already ends in 'ing', in which case add 'ly' instead.
-    If the string length is less than 3, leave it unchanged. Return
-    the resulting string.
+    last = s[-3:]
+    if len(s) > 2:
+        if last == "ing":
+            print s + "ly"
+        else:
+            print s + "ing"
+    else:
+        print s
 
-    >>> verbing('hail')
-    'hailing'
-    >>> verbing('swiming')
-    'swimingly'
-    >>> verbing('do')
-    'do'
-    """
-    raise NotImplementedError
+string = raw_input("Enter a string: ")
+verbing(string)
 
 
 def not_bad(s):
-    """
-    Given a string, find the first appearance of the substring 'not'
-    and 'bad'. If the 'bad' follows the 'not', replace the whole
-    'not'...'bad' substring with 'good'. Return the resulting string.
-    So 'This dinner is not that bad!' yields: 'This dinner is
-    good!'
+  notindex = s.find('not')
+  if notindex != -1:
+    if s.find('bad') != -1:
+      badindex = s.find('bad') + 3
+      if notindex < badindex:
+        removetext = s[notindex:badindex]
+        print s.replace(removetext, 'good')
+  else:
+      print s
 
-    >>> not_bad('This movie is not so bad')
-    'This movie is good'
-    >>> not_bad('This dinner is not that bad!')
-    'This dinner is good!'
-    >>> not_bad('This tea is not hot')
-    'This tea is not hot'
-    >>> not_bad("It's bad yet not")
-    "It's bad yet not"
-    """
-    raise NotImplementedError
+string = raw_input("Enter a phrase: ")
+not_bad(string)
 
 
 def front_back(a, b):
-    """
-    Consider dividing a string into two halves. If the length is even,
-    the front and back halves are the same length. If the length is
-    odd, we'll say that the extra char goes in the front half. e.g.
-    'abcde', the front half is 'abc', the back half 'de'. Given 2
-    strings, a and b, return a string of the form a-front + b-front +
-    a-back + b-back
+    bhalfa = -int(len(a)/2)
+    fhalfa = len(a) + bhalfa
+    fronta = a[:fhalfa]
+    backa = a[bhalfa:]
+    bhalfb = -int(len(b)/2)
+    fhalfb = len(b) + bhalfb
+    frontb = b[:fhalfb]
+    backb = b[bhalfb:]
+    print fronta + frontb + backa + backb
+    
 
-    >>> front_back('abcd', 'xy')
-    'abxcdy'
-    >>> front_back('abcde', 'xyz')
-    'abcxydez'
-    >>> front_back('Kitten', 'Donut')
-    'KitDontenut'
-    """
-    raise NotImplementedError
+stringa = raw_input("Enter a string: ")
+stringb = raw_input("Enter a string: ")
+front_back(stringa,stringb)
